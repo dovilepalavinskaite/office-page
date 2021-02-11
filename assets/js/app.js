9cd7502bf5e6ae1e4961765ff8c76d6113ef3d24
dovilepalavinskaite <palavinskaite.dovile@gmail.com>
2021-02-11 16:08:01 +0200

    Vanilla JS burger added with some animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 
+// Background smoothly changes when scrolled: 
+
 $(document).ready(function () { 
 	$(window).scroll(function () { 
 		if ($(document).scrollTop() > 100) 
@@ -8,3 +10,30 @@ $(document).ready(function () {
 		} 
 	}); 
 });
+
+
+// Burger menu functionality:
+
+const navSlider = () => {
+	const nav = document.querySelector('.nav-links');
+	const burger = document.querySelector('.burger');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active');
+
+	// Animation:
+		navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = '';
+		} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.8}s`;
+		}
+	});
+	// Burger lines animation:
+	burger.classList.toggle('toggle');
+	});
+	
+}
+
+navSlider();
